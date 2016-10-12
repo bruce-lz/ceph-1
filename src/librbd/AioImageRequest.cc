ceph/ceph

librbd: cache was not switching to writeback after first flush
Fixes: http://tracker.ceph.com/issues/16654
Signed-off-by: Jason Dillaman <dillaman@redhat.com>

src/librbd/AioImageRequest.cc
@@ -563,6 +563,8 @@ void AioImageDiscard<I>::update_stats(size_t length) {
 template <typename I>
 void AioImageFlush<I>::send_request() {
   I &image_ctx = this->m_image_ctx;
+  image_ctx.user_flushed();
+
   bool journaling = false;
   {
     RWLock::RLocker snap_locker(image_ctx.snap_lock);

bruce-lz/ceph-1

(cherry picked from commit 2d9840af39555ce00246b50e4a5c186798bd88ff)
Conflicts:
	src/librbd/AioImageRequest.cc: trivial resolution
Resolves: rhbz#1367891

src/librbd/AioImageRequest.cc
@@ -447,6 +447,8 @@ void AioImageDiscard::update_stats(size_t length) {
 }
 
 void AioImageFlush::send_request() {
+  m_image_ctx.user_flushed();
+
   bool journaling = false;
   {
     RWLock::RLocker snap_locker(m_image_ctx.snap_lock);